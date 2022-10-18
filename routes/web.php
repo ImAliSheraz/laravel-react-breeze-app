9fdd2dd38855201d0e1cbd12f44537fab3ba79ac
Ali Sheraz <imalisheraz@gmail.com>
2022-10-18 13:38:12 +0500

    📦 NEW: Add Post Backend MCR

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Inertia\Inertia;
 |
 */
 
+Route::resource('posts', PostController::class);
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
